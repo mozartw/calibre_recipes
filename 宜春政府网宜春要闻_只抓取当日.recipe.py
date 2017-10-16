aa4183ff83d15a03bbfba3ea705746863af5c106
YYTB <32561267+YYTB@users.noreply.github.com>
2017-10-16 17:30:52 +0800

    Update 宜春政府网宜春要闻_只抓取当日.recipe.py

--- 宜春政府网宜春要闻_只抓取当日.recipe.py
+++ 宜春政府网宜春要闻_只抓取当日.recipe.py
@@ -11,7 +11,8 @@ class yichunyaowen(BasicNewsRecipe):
 
     url_prefix = 'http://www.yichun.gov.cn/zwgk/zwdt/zwyw/'
     no_stylesheets = True
-    keep_only_tags = [{ 'class': 'Custom_UnionStyle' }]
+    keep_only_tags = [{ 'style': ' border:3px solid #f0f0f0;' }]
+    remove_tags = [dict(name='td', attrs={'style':'font-size:12px;'}),dict(name='td', attrs={'width':'50%'})] #移除上下‘更新时间’和‘信息来源’两个多余元素
 #    delay = 1
     simultaneous_downloads = 5
     remove_javascript = True
@@ -19,7 +20,7 @@ class yichunyaowen(BasicNewsRecipe):
     __author__ = 'suchao.personal@gmail.com' # 这个订阅列表的作者
 #   oldest_article = 1  #下载的最旧的文章是几天前的。默认是7天，单位是天。如果文章有日期，这个参数起作用。但是这个日期暂时不知道怎么认定，这个参数在宜春政府网的政务要闻不起作用
 
-    datetime = str(datetime.date.today()).split('-')  #对当天日期进行拆分，返回一个['2017', '10', '09']形式的列表
+    datetime_t = str(datetime.date.today()).split('-')  #对当天日期进行拆分，返回一个['2017', '10', '09']形式的列表
 
     def get_title(self, link):
         return link.contents[0].strip()
@@ -36,12 +37,11 @@ class yichunyaowen(BasicNewsRecipe):
             month = find_today.search(str(tr))  # 把上面构建的表达式作用于findAll找出来的内容
 
             try:
-                if not month.group(2) == '14':#self.datetime[2]:  # 判断日期是否为当日
-                    continue
-                if not month.group(1) == '10':#self.datetime[1]:  # 判断月份是否为当日
-                    continue
-                arti.append(str(tr))  # 注意要转换为字符串，beautifusoup不接受列表和其他类型的数据
-
+                d1 = datetime.date.today()  # 获取今天的日期
+                d2 = datetime.date(int(self.datetime_t[0]), int(month.group(1)), int(month.group(2)))  # 获取新闻的日期
+                days_betwen = (d1 - d2).days #获取时间差，结果为整数
+                if days_betwen <= 1 : #限定抓取几天内的新闻，当天的则为days_betwen == 0
+                    arti.append(str(tr))  # 注意要转换为字符串，beautifusoup不接受列表和其他类型的数据
             except:
                 pass
 
