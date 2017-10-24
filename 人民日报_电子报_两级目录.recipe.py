2ccbc335d6437b88f1ee1d3b0d82503b09d66b46
YYTB <suchao.personal@gmail.com>
2017-10-24 21:58:11 +0800

    改进人民日报抓取，改为二级标题，改进标题查找正则表达式的效率

--- 人民日报_电子报_两级目录.recipe.py
+++ 人民日报_电子报_两级目录.recipe.py
@@ -50,17 +50,15 @@ class renmindaily(BasicNewsRecipe):
     """
     本来大部分网站生成标题一般找出正文链接的<a>（定义为link）标签后，就可以通过link.contents[0].strip()来获得正文标题
     但是人民日报<a>标签形式是“<a href="nw.D110000renmrb_20171016_2-01.htm"><script>document.write(view("中共中央召开党外人士座谈会<br>征求对中共十九大报告的意见  "))</script></a>”这种多了script脚本等多余内容的形式
-    本来在pycharm测试当中可以用“link2.contents[0].contents[0].lstrip(r'document.write(view("').rstrip(r'"))').strip().replace('<br>', ' ')”也可以取得简洁的标题
+    本来在pycharm测试当中可以用“link.contents[0].contents[0].lstrip(r'document.write(view("').rstrip(r'"))').strip().replace('<br>', ' ')”也可以取得简洁的标题
     但是在calibre当中总是报错，或者总在运行当中，但无法抓取任何内容，也不提示超时，原因不明
-    万般无奈，只能把link（在这里定义为link2）转换为str字符串，然后用正则表达式对冗余内容进行替换（现在想起来有些复杂了，完全用replace()就行，不过写完了就算了吧）
+    万般无奈，只能把link转换为str字符串，然后用正则表达式进行查找并引用找到的编组
     经验：这里定义的link为beautisoup返回的特殊对象，在calibre当中有特别的识别方法，有一些特别的而要求。如果用str()转换为字符串后则没有特别要求。事实上我用return任意一个‘字符串’都能运行，只不过抓取的文章标题全部是'字符串'
     """
-    def get_title(self, link2):
-        lchange = re.compile(r'<a.*\("') # 构建左侧查找冗余内容的正则表达式，注意对括号进行转义
-        rchange = re.compile(r'"\).*</a>') # 构建右侧查找冗余内容的正则表达式，注意对括号进行转义
-        lre = lchange.sub('',str(link2)) # 替换左侧内容
-        rre = rchange.sub('',lre) # 替换右侧内容
-        return rre.strip().replace('<br>', ' ') #返回最终结果并进一步优化，在recipe的最后给文章标题提供值
+    def get_title(self, link):
+        find_title = re.compile(r'<.*><script>document\.write\(view\("(.*)"\)\)</script></a>') # 构建正则表达式，注意对括号进行转义
+        search_title = find_title.search(str(link)) # 查找内容
+        return search_title.group(1).replace('<br>', ' ').strip() #返回最终结果并进一步优化，在recipe的最后给文章标题提供值
 
 
     #下面的函数为recipe必要函数，返回的内容直接用于生成电子书
@@ -68,36 +66,30 @@ class renmindaily(BasicNewsRecipe):
         #下面的for循环用soupfind找到各版面的url并生成列表，带pdf的链接抛弃
         soup = self.index_to_soup(self.url_prefix_add2)
         banmiankuai = soup.find('div',{'id':'pageList'}) #可以有多个属性，比如'table',{'cellpadding':'2','width':'100%'}
-        urlist = [] #各版面链接
+
         ans0 = []
+        #下面的for循环依次遍历各个版面的url并生成二级内容
         for link in banmiankuai.findAll('a'):
+            articles = []
             if 'pdf' in link['href']:
                 continue
+            soup = self.index_to_soup(self.url_prefix_add + link['href'].lstrip(r'./'))
             vol_title = link.contents[0].strip()
-            urlist.append(self.url_prefix_add + link['href'].lstrip(r'./'))
-            #这个articles列表必须放在这个位置，放下下面的for循环里面会造成最终结果缺少东西，试了很多次的结果，原因待分析
-            articles = []
-            #下面的for循环用于给soup.find提供多个参数,即包含最终文章的链接网页框架
-            for ur in urlist:
+            div = soup.find('div',{'id':'titleList'})#抓取的正文链接框架部分
 
-                soup = self.index_to_soup(ur)
-                div = soup.find('div',{'id':'titleList'})#抓取的正文链接框架部分
+            for link in div.findAll('a'):
+                videolink = re.compile(r'src="')
+                vlinkfind = videolink.findall(str(link))
 
+                if not vlinkfind:
+                    til = self.get_title(link)
+                    url = self.url_prefix_add + link['href']
+                    a = { 'title':til , 'url': url }
 
-                for link2 in div.findAll('a'):
+                    articles.append(a)
 
-                    videolink = re.compile(r'src="')
-                    vlinkfind = videolink.findall(str(link2))
+            ans = (vol_title, articles)
 
-                    if not vlinkfind:
-                        til = self.get_title(link2)
-                        url = self.url_prefix_add + link2['href']
-                        a = { 'title':til , 'url': url }
-
-                        articles.append(a)
-
-                        ans = [('vol_title', articles)]
-
-                        ans0.append(ans)
+            ans0.append(ans)
 
         return ans0
