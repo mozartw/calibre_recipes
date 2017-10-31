7da4e5ee93960fc6facce965f4f62b51d8e30a91
YYTB <32561267+YYTB@users.noreply.github.com>
2017-10-31 16:05:56 +0800

    Update 法国国际广播电台.recipe.py

--- 法国国际广播电台.recipe.py
+++ 法国国际广播电台.recipe.py
@@ -6,13 +6,12 @@ class renmindaily(BasicNewsRecipe):
 
     title = '法国国际广播电台'
     description = '抓取法国国际广播电台中文网各栏目新闻'
-	no_stylesheets = True #不采用页面样式表
-    keep_only_tags = [{ 'class': 'text_c' }] #保留的正文部分
+    no_stylesheets = True #不采用页面样式表
+    keep_only_tags = [{ 'class': 'article-page' }] #保留的正文部分
+    remove_tags = [dict(name='div', attrs={'class':'soc-connect clearfix'}),dict(name='div', attrs={'class':'actions'}),dict(name='div', attrs={'class':'meta'})] #移除上下多余元素
+    extra_css = 'h1 { font-size: xx-large;}  h2 { font-size: large;}' #抓出来的文章标题太大，把字体改小一点
+    #delay = 1 #网页访问较慢（fuck！！），设置1秒延时
 
-    #extra_css = 'h1 { font-size: xx-large;}  h2 { font-size: large;}' #抓出来的文章标题太大，把字体改小一点，在人民日报网页中标题是h1标签
-    #delay = 1
-    #10线程下载
-    simultaneous_downloads = 10
     remove_javascript = True
     max_articles_per_feed  = 999 #最大文章数，默认为100
     #压缩图片
@@ -26,8 +25,8 @@ class renmindaily(BasicNewsRecipe):
 
     #下面的函数为recipe必要函数，返回的内容直接用于生成电子书
     def parse_index(self):
-		liebie_dic = {'中国':'%E4%B8%AD%E5%9B%BD','港澳台':'%E6%B8%AF%E6%BE%B3%E5%8F%B0', '国际':'%E5%9B%BD%E9%99%85', '人权':'%E4%BA%BA%E6%9D%83', '政治':'%E6%94%BF%E6%B2%BB', '经贸':'%E7%BB%8F%E8%B4%B8', '社会':'%E7%A4%BE%E4%BC%9A', '生态':'%E7%94%9F%E6%80%81', '科技与文化':'%E7%A7%91%E6%8A%80%E4%B8%8E%E6%96%87%E5%8C%96'}
-		liebie_list = ['中国','港澳台']
+		liebie_dic = {'中国':'%E4%B8%AD%E5%9B%BD','港澳台':'%E6%B8%AF%E6%BE%B3%E5%8F%B0', '国际':'%E5%9B%BD%E9%99%85', '人权':'%E4%BA%BA%E6%9D%83', '政治':'%E6%94%BF%E6%B2%BB', '经贸':'%E7%BB%8F%E8%B4%B8', '社会':'%E7%A4%BE%E4%BC%9A', '生态':'%E7%94%9F%E6%80%81', '科技与文化':'%E7%A7%91%E6%8A%80%E4%B8%8E%E6%96%87%E5%8C%96'} # 栏目及对应的url编码
+		liebie_list = ['中国','港澳台','国际','人权','政治','经贸','社会','生态','科技与文化'] #在上面的字典范围内设置需要抓取的栏目
 		
 		ans0 = []
 		for lan in liebie_list:
